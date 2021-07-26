8f2d79ddc12e00ae475501379ca25bcd8273d72b
Igor <igor.gutoo63@gmail.com>
2021-07-26 20:21:20 -0300

    Exercícios aula 22

---
+++ Atividades/exercicios/laravel/sistema/resources/views/produto/index.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <section class="col-lg-7 connectedSortable">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    <i class="fas fa-chart-pie mr-1"></i>
+                                    Produtos
+                                </h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="tab-content p-0">
+                                    @foreach ($produtos as $produto)
+                                        <ul class="list">
+                                            <li>Nome: {{ $produto->nome }}</li>
+                                            <li>Unidade: {{ $produto->um }}</li>
+                                            <hr>
+                                        </ul>
+                                    @endforeach
+                                    <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                                        <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
--- Atividades/exercicios/laravel/sistema/routes/web.php
+++ Atividades/exercicios/laravel/sistema/routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Models\Produto;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/produtos', function () {
+    $produtos = Produto::all();
+    return view('produto.index', ['produtos' => $produtos]);
+});
+
+Route::get('/produto/{id}', function ($id) {
+    $produto = Produto::findOrFail($id);
+    return view('produto.show', ['produto' => $produto]);
 });

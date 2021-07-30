a6a40bbdb78c24e070f28b7e65245f0fb9ec0ebd
Igor <igor.gutoo63@gmail.com>
2021-07-29 21:48:05 -0300

    Adicionando página de Login no sistema

--- Projeto/web/src/App.jsx
+++ Projeto/web/src/App.jsx
@@ -1,19 +1,12 @@
-import './App.css';
+import Login from './components/Login';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Login></Login>
+    </>
   );
 }
 
